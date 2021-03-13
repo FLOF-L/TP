15c945e671450901760e2b4e770a4d44aeef2b8c
Felipe Luís de Oliveira Fernandes <77802799+FLOF-L@users.noreply.github.com>
2021-03-13 15:03:05 -0300

    Create decisões.cs

---
+++ decisões.cs
@@ -0,0 +1,181 @@
+using System;
+
+namespace TPAula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //Ex 1 Entrar via teclado, com dois valores distintos. Exibir o
+            //maior deles. 
+
+            //int a, b, maior;
+            //Console.Write("Digite o valor de 'A': ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Digite o valor de 'B': ");
+            //b = int.Parse(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    maior = a;
+            //}
+            //else
+            //{
+            //    maior = b;
+            //}
+            //Console.WriteLine("O maior valor é {​​0}​​", maior);
+
+
+            //Ex 2 Entrar via teclado, com dois valores distintos. Exibir o
+            //menor deles.
+
+            //int a, b, menor;
+            //Console.Write("Digite o valor de 'A': ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Digite o valor de 'B': ");
+            //b = int.Parse(Console.ReadLine());
+
+            //if (a < b)
+            //{
+            //    menor = a;
+            //}
+            //else
+            //{
+            //    menor = b;
+            //}
+            //Console.WriteLine("O menor valor é {0}", menor);
+
+
+            //Ex 3 Entrar com dois valores quaisquer. Exibir o maior
+            //deles, se existir, caso contrário, enviar mensagem
+//avisando que os números são idênticos. 
+            //int x, y;
+            //Console.Write("Digite o primeiro valor : ");
+            //x = int.Parse(Console.ReadLine());
+            //Console.Write("Digite o segundo valor : ");
+            //y = int.Parse(Console.ReadLine());
+
+            //if (x == y)
+            //{
+            //    Console.WriteLine("Os números são idênticos");
+            //}
+            //if (x > y)
+            //{
+            //    Console.WriteLine("O maior deles é {0}", x);
+            //}
+            //if (y > x)
+            //{
+            //    Console.WriteLine("O maior deles é {0}", y);
+
+
+            //Ex 4
+
+            //float B, A, area;
+            //Console.Write("Qual a base do retângulo(cm)? ");
+            //B = float.Parse(Console.ReadLine());
+            //Console.Write("Qual a altura do retângulo(cm?) ");
+            //A = float.Parse(Console.ReadLine());
+            //area = B * A;
+
+            //Console.WriteLine("A area do terrno é de {0}m²", area);
+
+            //if(area >= 100)
+            //{
+            //    Console.WriteLine("Terreno Grande");
+            //}
+
+            //Ex 5
+
+            //float B, A, area;
+            //Console.Write("Qual a base do retângulo(cm)? ");
+            //B = float.Parse(Console.ReadLine());
+            //Console.Write("Qual a altura do retângulo(cm?) ");
+            //A = float.Parse(Console.ReadLine());
+            //area = B * A;
+
+            //Console.WriteLine("A area do terrno é de {0}m²", area);
+
+            //if (area >= 100)
+            //{
+            //    Console.WriteLine("Terreno Grande");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Terreno Pequeno");
+            //}
+
+            //Ex 6
+            //int a, b, c;
+            //Console.Write("Digite o valor 1: ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Digite o valor 2: ");
+            //b = int.Parse(Console.ReadLine());
+            //Console.Write("Digite o valor 3: ");
+            //c = int.Parse(Console.ReadLine());
+
+            //if (a > b)
+            //{
+            //    if (a > c)
+            //    {
+            //        Console.WriteLine("O maior é {0}", a);
+            //    }
+            //}
+
+            //if (b > a)
+            //{
+            //    if (b > c)
+            //    {
+            //        Console.WriteLine("O maior é {0}", b);
+            //    }
+            //}
+
+            //if (c > b)
+            //{
+            //    if (c > a)
+            //    {
+            //        Console.WriteLine("O maior é {0}", c);
+            //    }
+            //}
+
+            //Ex 7
+            //float p1, p2, m;
+            //Console.Write("Digite a nota P1: ");
+            //p1 = float.Parse(Console.ReadLine());
+            //Console.Write("Digite a nota P2: ");
+            //p2 = float.Parse(Console.ReadLine());
+
+            //m = (p1 + (2 * p2)) / 3;
+
+            //if (m >= 5)
+            //{
+            //    Console.WriteLine("Aprovado!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Reprovado...");
+            //}
+
+            //Ex 8
+            //double p1,p2, m;
+            //Console.Write("Digite a nota P1: ");
+            //p1 = float.Parse(Console.ReadLine());
+
+            //p2 = (15 - p1) / 2;
+
+
+
+            //Console.WriteLine("Com a média sendo 5  e tirando {0} na P1, você deve obter pelo menos {1} na P2.",p1, p2);
+
+
+
+
+
+
+
+        }
+    }
+    }
+
+
