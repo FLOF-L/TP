9fde09dc02b24e84cd9db23450c8c7f074ce5b3b
Felipe Luís de Oliveira Fernandes <77802799+FLOF-L@users.noreply.github.com>
2021-04-19 08:33:22 -0300

    Create Loop1.cs

---
+++ Loop1.cs
@@ -0,0 +1,228 @@
+using System;
+namespace EX12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Loop While
+
+
+            //Ex 1
+
+            //int num = 0;
+
+            //while (num <= 0)
+            //{
+            //    Console.WriteLine("O valor deve ser positivo!");
+            //    Console.Write("Digite novamente: ");
+            //    num = int.Parse(Console.ReadLine());
+
+
+
+            //Ex 2
+            //int n1, n2;
+            //Console.Write("Valor 1: ");
+            //n1 = int.Parse(Console.ReadLine());
+            //Console.Write("Valor 2: ");
+            //n2 = int.Parse(Console.ReadLine());
+
+            //while( n2 <= n1)
+            //{
+            //    Console.Write("Valor 2: ");
+            //    n2 = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("OK");
+
+
+            //Ex 3
+            //char sexo;
+            //Console.Write("Sexo: ");
+            //sexo = char.Parse(Console.ReadLine());
+
+            //while (sexo!= 'M' && sexo != 'F')
+            //{
+            //    Console.Write("Sexo: ");
+            //    sexo = char.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("OK");
+
+
+
+            //Ex 4
+            //int contador = 0;
+            //int num = 5;
+
+            //while (contador < 10)
+            //{
+            //    contador = contador + 1;
+            //    Console.WriteLine("{0} x {1} = {2}", contador, num, contador * num);
+
+            //}
+
+
+            //Ex 5 
+
+            //int n,contador;
+            //Console.Write("Digite um valor: ");
+            //n = int.Parse(Console.ReadLine());
+            //contador = 0;
+            //while (n<=0)
+            //{
+            //    Console.Write("Digite um valor POSITIVO: ");
+            //    n = int.Parse(Console.ReadLine());
+
+            //}
+
+            //while (contador <  10)
+            //{
+            //    contador++;
+            //    Console.WriteLine("{0} x {1} = {2}", n, contador, n * contador);
+            //}
+
+
+            //Ex 6
+            //int x, a, b;
+
+            //Console.Write("Digite um valor positivo: ");
+            //x = int.Parse(Console.ReadLine());
+
+            //while (x < 0)
+            //{
+            //    Console.Write("Digite um valor positivo: ");
+            //    x = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("Em qual intervalo a tabuada deve ser calculada?");
+            //Console.Write("Valor de início: ");
+            //a = int.Parse(Console.ReadLine());
+            //Console.Write("Valor final: ");
+            //b = int.Parse(Console.ReadLine());
+
+
+            //while (a > b)
+            //{
+            //    Console.Write("Valor final: ");
+            //    b = int.Parse(Console.ReadLine());
+            //}
+
+
+            //while (b > a)
+            //{
+
+            //    Console.WriteLine("{0} x {1} = {2}", x, b, x * b);
+            //}
+
+
+
+
+
+            //Ex 7
+
+            //int n, contador;
+            //contador = 0;
+            //n =0;
+
+            //while (n <= 20)
+            //{
+
+            //    n = n + 1;
+
+
+
+            //    Console.ReadKey();
+            //    Console.WriteLine("____________________________");
+            //    Console.WriteLine("Tabuada do {0}", n);
+
+            //    contador = 0;
+            //    while (contador < 10)
+            //    {
+            //        contador = contador + 1;
+            //        Console.WriteLine("{0} x {1} = {2}", n, contador, n * contador);
+            //    }
+
+            //}
+
+
+
+
+            //Ex 8
+
+            //int n, soma;
+
+            //n = 0;
+            //soma = 0;
+
+            //while (n <= 99)
+            //{
+            //    n++;
+            //    soma = soma + n;
+            //}
+
+            //Console.WriteLine("A soma dos números inteiros de 1 até 100 é {0}",soma);
+
+
+
+            //Ex 9
+
+
+            //int contador, maior, menor, n;
+
+            //Console.WriteLine("Digite 10 números");
+            //contador = 0;
+            //menor = 0;
+            //maior = 0;
+
+
+            //while (contador < 10)
+            //{
+            //    n = int.Parse(Console.ReadLine());
+            //    contador++;
+
+            //    if (contador == 1)
+            //    {
+            //        maior = n;
+            //        menor = n;
+            //    }
+
+
+
+            //    if (n > maior)
+            //    {
+            //        maior = n;
+            //    }
+            //    else
+            //    {
+            //        if (n < menor)
+            //        {
+            //            menor = n;
+            //        }
+            //    }
+
+            //}
+
+            //Console.WriteLine("Maior número : {0}/ Menor número: {1}", maior, menor);
+
+
+            //Ex 10
+
+            //int a, b, c, contador;
+            //contador = 3;
+            //a = 1;
+            //b = 1;
+
+
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //while (contador <= 30)
+            //{
+            //    c = a + b;
+            //    Console.WriteLine(c);
+
+            //    a = b;
+            //    b = c;
+            //    contador++;
+
