3e451e8bd7466ff4ab6974b34faaf2c2801586f5
Felipe Luís de Oliveira Fernandes <77802799+FLOF-L@users.noreply.github.com>
2021-05-08 11:47:31 -0300

    Create Ex_extra.cs

---
+++ Ex_extra.cs
@@ -0,0 +1,115 @@
+//Exercícios do URI JUGDE para treino
+
+            /*Leia 6 valores que podem ser números de ponto flutuante. Depois imprima quantos deles foram positivos. 
+             * Na próxima linha, imprima a média de todos os valores positivos digitados, com um dígito após a vírgula decimal.*/
+
+
+            //float n,porcentagem_pos;
+
+            //n = 0;
+            //int contador = 0;
+            //float soma = 0;
+            //int qtd_positivos;
+            //qtd_positivos = 0;
+            //float media = 0;
+
+            //do
+            //{
+            //    Console.Write("Digite um número: ");
+            //    n = float.Parse(Console.ReadLine());
+
+            //    if (n > 0)
+            //    {
+            //        qtd_positivos = qtd_positivos + 1;
+            //    }
+
+            //    soma = soma + n;
+            //    contador++;
+
+            //} while (contador < 6);
+
+
+            //media = soma / 6;
+
+
+            //porcentagem_pos = (qtd_positivos / (float)6)* 100 ;
+
+            //Console.WriteLine("A média é {0:f3}", media);
+            //Console.WriteLine("A porcentagem de positivos é {0:f1}%", porcentagem_pos);
+
+
+
+
+
+            //Exercício URI JUDGE
+            //Leia um inteiro N e calcule todos os seus divisores.
+
+            //int n;
+            //Console.Write("Digite um número: ");
+            //n = int.Parse(Console.ReadLine());
+
+            //int divisor = 1;
+            //int resto = 0;
+
+            //do
+            //{
+            //    resto = n % divisor;
+
+            //    if (resto == 0)
+            //    {
+            //        Console.WriteLine(divisor);
+            //    }
+
+
+            //    divisor++;
+
+
+
+            //} while (divisor <= n);
+
+
+
+
+            //Exercício URI JUDGE
+            /*O programa deve ler um número inteiro X indefinidamente (parar quando X = 0 ). 
+             * Para cada X , imprima a soma de cinco números pares consecutivos de X , incluindo se X for par. 
+             * Se o número de entrada for 4, por exemplo,
+             * a saída deve ser 40, esse é o resultado da operação: 4 + 6 + 8 + 10 + 12. 
+             * Se o número de entrada for 11, por exemplo, a saída deve ser 80, que é o resultado de 12 + 14 + 16 + 18 + 20.*/
+
+            //int x, soma, contador;
+            //contador = 5;
+            //soma = 0;
+            //int num_par;
+
+
+            //Console.Write("Digite o Número:");
+            //x = int.Parse(Console.ReadLine());
+            //while (contador > 0)
+            //{
+            //    num_par = x;
+            //    if (num_par % 2 == (int)0)
+            //    {
+
+            //        soma = num_par + soma;
+            //        x = x + 2;
+
+
+            //    }
+
+            //    if (x % 2 != (int)0)
+            //    {
+            //        if (contador == 5)
+            //        {
+            //            x = x + 1;
+            //        }
+
+            //        soma = x + soma;
+            //        x = x + 2;
+
+
+            //    }
+
+            //    contador--;
+            //}
+            //Console.WriteLine(soma);
