8c1227c2d99c5473f545b14f73764f14c338ac5b
Felipe Luís de Oliveira Fernandes <77802799+FLOF-L@users.noreply.github.com>
2021-05-17 10:18:09 -0300

    Create do_while_treino.cs

---
+++ do_while_treino.cs
@@ -0,0 +1,239 @@
+
+            /* Desenvolva  um  programa  que  calcule  e  exiba  no monitor  o  produto  dos  números  inteiros  múltiplos  de  9 menores que 39*/
+
+            //int multiplo, resultado;
+            //multiplo = 9;
+            //resultado = 0;
+            //int n = 1;
+
+
+
+            //do
+            //{
+            //    Console.WriteLine("{0} x {1} : {2}", multiplo, n, resultado);
+            //    resultado = multiplo * n;
+
+            //    n++;
+
+            //} while (resultado <= 39);
+
+
+
+
+
+
+
+
+
+
+
+            /*.Elabore um  algoritmo  que  escreva  todos  os  números múltiplos de 7 entre 1 e 100.*/
+
+
+            //int multiplo, resultado;
+            //multiplo = 7;
+            //resultado = multiplo;
+            //int n = 1;
+
+
+            //do
+            //{
+
+            //    Console.WriteLine("{0} x {1} : {2}", multiplo, n, resultado);
+            //    n++;
+            //    resultado = multiplo * n;
+
+
+
+            //} while (resultado <= 100);
+
+
+
+
+
+
+
+
+
+
+
+
+            /*.Descreva  um  programa  que receba  
+             * via  teclado  50 valores que  são digitados e a cada  5  valores  exiba  a soma parcial deles.*/
+
+            //int contador = 0;
+
+            //int n = 0;
+            //int soma1 = 0;
+            //int soma2 = 0;
+            //int soma3 = 0;
+            //int soma4 = 0;
+            //int soma5 = 0;
+            //int soma6 = 0;
+            //int soma7 = 0;
+            //int soma8 = 0;
+            //int soma9 = 0;
+            //int soma10 = 0;
+
+            //do
+            //{
+
+
+
+            //    while (contador < 5)
+            //    {
+            //        contador++;
+            //        n = int.Parse(Console.ReadLine());
+            //        soma1 = n + soma1;
+
+
+            //        if (contador == 5)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma1);
+            //        }
+            //    }
+
+
+            //    while (contador >= 5 && contador < 10)
+            //    {
+            //        contador++;
+            //        n = int.Parse(Console.ReadLine());
+            //        soma2 = n + soma2;
+
+
+            //        if (contador == 10)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma2);
+            //        }
+            //    }
+
+            //    while (contador >= 10 && contador < 15)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma3 = n + soma3;
+            //        contador++;
+
+            //        if (contador == 15)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma3);
+            //        }
+            //    }
+
+            //    while (contador >= 15 && contador <= 20)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma4 = n + soma4;
+            //        contador++;
+
+            //        if (contador == 20)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma4);
+            //        }
+            //    }
+
+
+            //    while (contador >= 20 && contador <= 25)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma5 = n + soma5;
+            //        contador++;
+
+            //        if (contador == 25)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma5);
+            //        }
+            //    }
+
+            //    while (contador >= 25 && contador <= 30)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma6 = n + soma6;
+            //        contador++;
+
+            //        if (contador == 30)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma6);
+            //        }
+            //    }
+
+            //    while (contador >= 30 && contador <= 35)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma7 = n + soma7;
+            //        contador++;
+
+            //        if (contador == 35)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma7);
+            //        }
+            //    }
+
+            //    while (contador >= 35 && contador <= 40)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma8 = n + soma8;
+            //        contador++;
+
+            //        if (contador == 40)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma8);
+            //        }
+            //    }
+
+            //    while (contador >= 40 && contador <= 45)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma9 = n + soma9;
+            //        contador++;
+
+            //        if (contador == 45)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma9);
+            //        }
+            //    }
+
+            //    while (contador >= 45 && contador <= 50)
+            //    {
+            //        n = int.Parse(Console.ReadLine());
+            //        soma10 = n + soma10;
+            //        contador++;
+
+            //        if (contador == 50)
+            //        {
+            //            Console.WriteLine("A soma é : {0}", soma10);
+            //        }
+            //    }
+
+
+            //} while (contador <= 50);
+
+
+
+
+
+
+
+
+
+            /* 14.Calcule  e exiba  no monitor  a soma  dos  20  primeiros termos da série:
+             * 1/2    2/3    3/4    4/5    */
+
+
+            //double x = 1.0;
+            //double soma = 0;
+            //double conta = 0;
+            //int n = 1;
+            //int limite = 2;
+
+
+            //do
+            //{
+            //    conta = x / (x + 1);
+            //    soma = soma + conta;
+            //    conta = 0;
+            //    x++;
+            //    n++;
+
+            //} while (n <= limite);
+
+            //Console.WriteLine(soma);
