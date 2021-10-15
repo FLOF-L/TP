6c523466286dee858fc6eb73d6cbf50ddc62979e
Felipe Luís de Oliveira Fernandes <77802799+FLOF-L@users.noreply.github.com>
2021-10-15 09:00:49 -0300

    Create switch.cs

---
+++ switch.cs
@@ -0,0 +1,18 @@
+  static void Main(string[] args)
+        {
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Opção 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Opção 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Opção 3");
+                    break;
+            }
+
+        }
